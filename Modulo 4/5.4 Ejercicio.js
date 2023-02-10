43f54f78a71539e88efffc6fd4b6575bf0bae253
JhonIsNotReal <110693901+Jhonisnotreal@users.noreply.github.com>
2023-02-09 21:16:13 -0600

    Add files via upload

---
+++ Modulo 4/5.4 Ejercicio.js
@@ -0,0 +1,25 @@
+//Ejercicio 5.4
+//Mi altura en cm
+
+let altura = parseInt(1.70);
+console.log(altura);
+
+//Altura en metros con decimal
+let estaturaEnCm = parseFloat(1.7);
+console.log(estaturaEnCm);
+
+//Peso en kg
+let peso = 70;
+console.log(peso);
+
+//altura en metros redondeada hacia arriba
+estaturaEnMs = 1.70;
+console.log(estaturaEnMs.toFixed(1))
+
+//altura en ms redondeada hacia abajo
+let estaturaEnMs2 = 1.70;
+console.log(estaturaEnMs2.toPrecision(2))
+
+//el max valor que se puede obtener en JS
+let maxValor = 2 ** 1023;
+console.log("El maximo valor que se puede obtener en JS + 1 es: " + maxValor);
